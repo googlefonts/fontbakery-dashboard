e90f963fbc587541379ca7fe9d1db17345716bf6
Lasse Fister <commander@graphicore.de>
2017-06-02 04:32:20 +0200

    dnd-server: clean up, better logging and error handling, restructurind

--- containers/fbdraganddrop/lib/server/main.js
+++ containers/fbdraganddrop/lib/server/main.js
@@ -11,19 +11,48 @@ const express = require('express')
   , path = require('path')
   , bodyParser = require('body-parser')
   , StringDecoder = require('string_decoder').StringDecoder
-
   // https://github.com/squaremo/amqp.node
   , amqplib = require('amqplib')
   ;
 
 var ROOT_PATH = __dirname.split(path.sep).slice(0, -1).join(path.sep);
 
+function Logging(loglevel) {
+    this._numericLoglevel = this._levels[loglevel];
+}
+Logging.prototype._levels = {};
+
+([
+  , ['DEBUG', 10, console.info]
+  , ['INFO', 20, console.info]
+  , ['WARNING', 30, console.warn]
+  , ['ERROR', 40, console.error]
+  , ['CRITICAL', 50, console.error]
+]).forEach(function(setup) {
+    // method names: debug, info, warning, etc..
+    var loglevel = setup[0]
+      , method = loglevel.toLowerCase()
+      , numeric = setup[1]
+      , log = setup[2]
+      ;
+    Logging.prototype._levels[loglevel] = numeric;
+    Logging.prototype[method] = function() {
+        if(numeric < this._numericLoglevel)
+            return;
+        var args = [loglevel], i, l;
+        for(i=0,l=arguments.length;i<l;i++)
+            args.push(arguments[i]);
+        log.apply(null, args);
+    };
+});
+
 /**
  * Using a class here, so state variables are managed not on module level
  * Initialization starts the server.
  *
  */
-function Server(portNum,  amqpSetup, rethinkSetup) {
+function Server(logging, portNum,  amqpSetup, rethinkSetup) {
+    this._log = logging;
     this._portNum = portNum;
     this._dbName = rethinkSetup.db;
     this._dbTable = 'draganddrop';
@@ -31,24 +60,26 @@ function Server(portNum,  amqpSetup, rethinkSetup) {
     this._server = http.createServer(this._app);
     this._sio = socketio(this._server);
 
-
     this._r = rethinkdbdash(rethinkSetup);
+
     this._amqpConnection = null;
+    this._queueName = 'drag_and_drop_queue';
     // Start serving when the database and rabbitmq queue is ready
     Promise.all([
-            this._initDB()
-          , this._initAmqp()
-    ]).then(this.listen.bind(this))
-    .catch(function(err){
-        console.error('Can\'t initialize server.', err)
+                 this._initDB()
+               , this._initAmqp()
+               ])
+    .then(this._listen.bind(this))
+    .catch(function(err) {
+        this._log.error('Can\'t initialize server.', err);
         process.exit(1);
-    });
+    }.bind(this));
 
     this._app.get('/', this.fbDNDIndex.bind(this));
     this._app.use('/static', express.static('lib/static'));
     this._app.get('/report/:docid', this.fbDNDReport.bind(this));
 
-    this._app.use('/runchecks',bodyParser.raw(
+    this._app.use('/runchecks', bodyParser.raw(
                     {type: 'application/octet-stream', limit: '15mb'}));
     this._app.post('/runchecks', this.fbDNDReceive.bind(this));
     this._sio.on('connection', this.fbDNDSocketConnect.bind(this));
@@ -85,10 +116,10 @@ _p._initDB = function() {
     return createDatabase()
         .then(createTable)
         .error(function(err) {
-            // it's not an error if the table already exists
-            console.error('Error while initializing database.', err);
+            // It's not an error if the table already exists
+            this._log.warning('Error while initializing database.', err);
             throw err;
-        });
+        }.bind(this));
 };
 
 _p._initAmqp = function() {
@@ -99,14 +130,14 @@ _p._initAmqp = function() {
                 this._amqpConnection = connection;
             }.bind(this))
             .catch(function(err) {
-                // it's not an error if the table already exists
-                console.error('Error while connecting to queue.', err);
+                this._log.error('Error while connecting to queue.', err);
                 throw err;
-            });
+            }.bind(this));
 };
 
-_p.listen = function() {
+_p._listen = function() {
     this._server.listen(this._portNum);
+    this._log.info('Listening to port', this._portNum);
 };
 
 /**
@@ -114,11 +145,10 @@ _p.listen = function() {
  *
  * CLIENT
  *  : say hello
- *  : initiate the d'n'd interface
- *  : post fonts
- *      : then init socketio connection for docid
- *        // change url to docid url
- *        // open socketio connecion with answer from post
+ *  : initiate the d'n'd interface to post fonts
+ *        on success
+ *        change url to docid url
+ *        init socketio connection for docid (with answer from post)
  */
 _p.fbDNDIndex = function(req, res) {
     return res.sendFile('static/html/drag-and-drop-client.html',
@@ -152,6 +182,10 @@ _p.unpack = function(data) {
     return result;
 };
 
+_p._query = function() {
+    return this._r.table(this._dbTable);
+};
+
 /**
  * GET docid
  *
@@ -160,29 +194,26 @@ _p.unpack = function(data) {
  * SERVER
  *   : test if docid exists
  * CLIENT
- *   :
+ *   : same as fbDNDIndex on success
  */
-_p.fbDNDReport = function(req, res) {
+_p.fbDNDReport = function(req, res, next) {
     var docid = req.param('docid');
-
-    console.log('fbDNDReport docid:', docid);
-    // TODO: if there's no docid with docid return 404
-
-    // at the client:
-    // <script src="/socket.io/socket.io.js"></script>
-    // The js library is at this route automatically
-
-    // this is the same as the index page after the docid was returned
-    // so maybe we need to initialize the client with the docid
-    // or something that is specific
-    return this.fbDNDIndex(req, res);
+    return this._query().getAll(docid).count()
+        .then(function(found) {
+            if(found)
+                // This is the same client as the index page,
+                // after the docid was returned
+                return this.fbDNDIndex.bind(this, req, res, next);
+            // answer 404: NotFound
+            return res.status(404).send('Not found');
+        }).catch(next);
 };
 
 /**
  * Almost straight copy from the clients Controller.js
  */
 function mergeArrays(arrays) {
-    var jobSize = arrays.reduce(function(prev, cur){
+    var jobSize = arrays.reduce(function(prev, cur) {
                                 return prev + cur.byteLength; }, 0)
       , result, i, l, offset
       ;
@@ -192,81 +223,88 @@ function mergeArrays(arrays) {
     return result;
 }
 
-_p._onDocCreated = function(req, res, dbResponse) {
-    console.log('_onDocCreated','dbResponse', dbResponse);
-    var docid = dbResponse.generated_keys[0];
-
-    console.log('sending response')
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({docid: docid, url: 'report/' + docid}));
-
-    console.log('creating amqp channel')
-
-    this._amqpConnection.createChannel().then(function(channel) {
-        console.log('got channel')
-        var exchange = ''
-          , queue = 'drag_and_drop_queue'
-          , options = {
-                        // TODO: do we need persistence here?
-                        persistent: true
-                        // this? , deliveryMode: true
-                    }
-            // a buffer
-          , content
-            // expecting doc id to be only ASCII chars
-          , docidArray = Uint8Array.from(docid,
-                                function(chr){ return chr.charCodeAt(0);})
-          , docidLen = new Uint32Array(1)
-          ;
-        docidLen[0] = docidArray.byteLength;
-
-        console.log('docidLen is ', docidArray.byteLength , 'and looks like this:', docidLen.buffer)
-
-        content = mergeArrays([docidLen, docidArray, req.body]);
-        function onAssert(){
-            console.log('sendToQueue', 'queue', queue, content.byteLength, 'Bytes', typeof content, content, content[0], content[1], content[2], content[3])
-            channel.sendToQueue(queue, content, options);
-            return channel.close();
+_p._onCreateAMQPChannel = function  (docid, payload, channel) {
+    this._log.debug('_onCreateAMQPChannel', docid);
+    var options = {
+            // TODO: do we need persistent here?
+            persistent: true
+            // this? , deliveryMode: true
         }
-        channel.assertQueue(queue, {durable: true}).then(onAssert)
-    })
-    .catch(function(err) {
-        console.error('Can\'t create channel.', err);
-        return;
-    });
+        // a buffer
+      , content
+        // expecting doc id to be only ASCII chars, because serializing
+        // higher unicode is not that straight forward.
+      , docidArray = Uint8Array.from(docid,
+                            function(chr){ return chr.charCodeAt(0);})
+      , docidLen = new Uint32Array(1)
+      ;
+    docidLen[0] = docidArray.byteLength;
+    this._log.debug('docidLen is', docidArray.byteLength
+                                                , 'for docid:', docid);
+
+    content = mergeArrays([docidLen, docidArray, payload]);
+    function onAssert() {
+        // jshint validthis:true
+        this._log.info('sendToQueue doc', docid, 'queue', this._queueName
+                                        , content.byteLength, 'Bytes');
+        return channel.sendToQueue(this._queueName, content, options);
+    }
+    return channel.assertQueue(this._queueName, {durable: true})
+           .then(onAssert.bind(this))
+           .finally(function(){ channel.close(); })
+           ;
+};
+
+_p._onDocCreated = function(req, res, next, dbResponse) {
+    this._log.debug('_onDocCreated','dbResponse', dbResponse);
+    var docid = dbResponse.generated_keys[0]
+      , payload = req.body
+      ;
+
+    this._log.debug('creating amqp channel');
+    return this._amqpConnection.createChannel()
+        .then(this._onCreateAMQPChannel.bind(this, docid, payload))
+        .then(function(){ return docid; })
+        .catch(function(err) {
+            this._log.warning('Can\'t create channel.', err);
+            throw err;
+        }.bind(this));
 };
 
 /**
- * POST AJAX
+ * POST AJAX, a blob
  * SERVER
  *   : create a testrun document => docid
- *   : with an in-progress status for each fontfile
- *   : with an in-progress status for the testrun document (?)
- *   : dispatch job for worker
- *   : return docid OR maybe the new URL? because we want to use
+ *   : dispatch job to worker
+ *   : return docid and the new URL. Because we want to use
  *         also the browser history.pushState api. Ideally the server
  *         would send how to change urls to the client, because the browser
- *         must do the static urls.
+ *         must do the static urls (though the client has at the moment some
+ *         understanding of this url when the site is visited directly, which
+ *         is not optimal)
  */
-_p.fbDNDReceive = function(req, res) {
-    console.log('fbDNDReceive', req.body.byteLength, 'Bytes')
+_p.fbDNDReceive = function(req, res, next) {
 
     // var files = this.unpack(req.body)
     var doc = {
            isFinished: false
-         , files: {}
          , created: new Date()
         }
       ;
-
-    this._r.table(this._dbTable)
-           .insert(doc)
+    function success(docid) {
+        //jshint validthis:true
+        this._log.debug('Sending response:', docid);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({docid: docid, url: 'report/' + docid}));
+    }
+    this._query().insert(doc)
             .run()
-            .then(this._onDocCreated.bind(this, req, res))
+            .then(this._onDocCreated.bind(this, req, res, next))
+            .then(success.bind(this))
             .error(function(err) {
-                console.error('Creating a doc failed ', err);
-            });
+                this._log.error('Creating a doc failed ', err);
+                next(err);
+            }.bind(this));
 };
 
 
@@ -292,33 +330,13 @@ _p.fbDNDReceive = function(req, res) {
 _p.fbDNDSocketConnect = function(socket) {
     // wait for docid request ...
     socket.on('subscribe-changes', function (data) {
-        console.log('fbDNDSocketConnectchanges subscription requested for', data);
+        this._log.info('fbDNDSocketConnectchanges subscription requested for', data);
         // simple sanitation, all strings are valid requests so far
         if(typeof data.docid !== 'string')
             // this is actually required
             data.docid = '';
         this._subscribeToDoc(socket, data);
     }.bind(this));
-
-
-
-    // We don't listen to client sent events at this point
-    // socket.on('event', function(data){});
-
-//    var changeFeed = this._r. QUERY(docid) .change; //( . . . this.fbDNDRethinkChangeFeed.bind(this) ).run();
-//  this._sockets.set(socket, changeFeed);
-
-
-//    socket.on('disconnect', function(){
-//        var changeFeed = this._sockets.get(socket);
-//        changeFeed.close();
-//
-//        // don't use close=true if only the namespace needs closing
-//        // var close=true
-//        // socket.disconnect(close)
-//
-//        this._sockets.remove(socket);
-//    });
 };
 
 /**
@@ -338,13 +356,13 @@ _p.fbDNDSocketConnect = function(socket) {
 _p._fbDNDRethinkChangeFeed = function(cursor, socket, channel, err, data) {
     if (err) {
         // TODO: Handle error
-        console.error('Fail receivng the changes feed.', err);
+        this._log.error('Fail receivng change feed data.', err);
         return;
     }
     socket.emit(channel, data);
-    if(data.isFinished) {
+    if(data.newVal && data.newVal.isFinished) {
         // don't use close=true if only the namespace needs closing
-        cursor.close(console.error.bind(console));
+        cursor.close(this._log.error.bind(this._log));
         var close=true;
         socket.disconnect(close);
     }
@@ -354,17 +372,16 @@ _p._fbDNDRethinkChangeFeed = function(cursor, socket, channel, err, data) {
  * data.docid is the rethink db document UUID.
  */
 _p._subscribeToDoc = function(socket, data) {
-    this._r.table(this._dbTable)
-        .get(data.docid)
-        .changes({includeInitial: true, squash:true})
+    this._query().get(data.docid)
+        .changes({includeInitial: true, squash: 0.3})
         .run(function(err, cursor) {
             if(err) {
-                console.error('Can\'t run query for changes.', err);
+                this._log.error('Can\'t acquire change feed.', err);
                 throw err;
             }
             cursor.each(this._fbDNDRethinkChangeFeed.bind(this
                                         , cursor, socket, 'changes'));
-        }.bind(this))
+        }.bind(this), {cursor: true});
 };
 
 
@@ -380,9 +397,11 @@ if (typeof require != 'undefined' && require.main==module) {
                         || process.env.BROKER
                         || 'amqp://localhost'
         }
+      , logging = new Logging(process.env.FONTBAKERY_LOG_LEVEL || 'INFO')
       ;
 
-    new Server(3000, amqpSetup, rethinkSetup);
+    logging.info('Init server ...');
+    new Server(logging, 3000, amqpSetup, rethinkSetup);
 }
 
 
