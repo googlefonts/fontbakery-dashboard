ac624ace3a451d4e3f8c61c78ba1fb2040cab9c1
Lasse Fister <commander@graphicore.de>
2017-12-18 19:29:46 +0100

    [javascript/node] ManifestServer add an async, promise based queue.

--- containers/base/javascript/node/util/ManifestServer.js
+++ containers/base/javascript/node/util/ManifestServer.js
@@ -13,6 +13,52 @@ const { initAmqp }= require('./getSetup')
   , timestamp_pb = require('google-protobuf/google/protobuf/timestamp_pb.js')
   ;
 
+
+// TODO: this is a nice helper for _Source.schedule as well!
+function AsynQueue() {
+    this._current = null;
+    this._thread = [];
+}
+AsynQueue.prototype._tick = function() {
+    if(!this._thread.length || this._current) {
+        return;
+    }
+
+    this._current = this._thread.pop();
+    this._current().then(() => {
+        this._current = null;
+        this._tick();
+    });
+};
+
+AsynQueue.prototype.schedule = function(job) {
+    var resolve, reject
+        // resolve, reject of the closure are
+      , jobPromise = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+        })
+      ;
+    this._thread.unshift(() => {
+        var result;
+        try {
+            result = job();
+            resolve(result);
+        } catch(err) {
+            reject(err);
+        }
+
+        return (result && typeof result.then === 'function')
+              // run next after result; no matter if result succeeds or fails
+            ? result.then(()=>null, ()=>null)
+              // run next queue.thread item asap
+            : Promise.resolve(null)
+            ;
+    });
+    this._tick();
+    return jobPromise;
+};
+
 /**
  * This connects the manifestSources to the world (cluster)
  *
@@ -37,9 +83,11 @@ function ManifestServer(logging, id, sources, port, cacheSetup, amqpSetup) {
     // this._manifestMasterRegisterSourceQueueName = 'fontbakery-manifest-master-register-source';
 
     this._sources = Object.create(null);
+    this._queues = new Map();
+    this.__queue = this._queue.bind(this);
 
     // Start serving when the database and rabbitmq queue is ready
-   Promise.all([
+    Promise.all([
                  initAmqp(this._log, amqpSetup)
                , this._cache.waitForReady()
                , this._addSources(sources)
@@ -52,11 +100,11 @@ function ManifestServer(logging, id, sources, port, cacheSetup, amqpSetup) {
         this._ready = true;
         this._log.info('Ready now!');
     })
-    .then(this.updateAll.bind(this))
     .catch(function(err) {
         this._log.error('Can\'t initialize server.', err);
         process.exit(1);
     }.bind(this))
+    .then(this.updateAll.bind(this))
    ;
 }
 
@@ -78,10 +126,13 @@ _p._registerSource = function(sourceID) {
 _p._addSource = function(source) {
     if(source.id in this._sources)
         throw new Error('Source ID "' + source.id + '" already exists.');
+
     source.setDispatchFamily(this._dispatchFamily.bind(this, source.id));
+    source.setQueue(this.__queue);
+
     this._sources[source.id] = source;
     this._registerSource(source.id);
-    return source.init();
+    return source.schedule('init');
 };
 
 _p.updateAll = function(force) {
@@ -92,7 +143,18 @@ _p.updateAll = function(force) {
 };
 
 _p.update = function(sourceId, force) {
-    return this._sources[sourceId].schedule('update', force);
+    return this._sources[sourceId].schedule('update', force)
+        .then(
+              () => {
+                  this._log.info('Finished updating ', sourceId);
+            }
+            , err => {
+                this._log.error('ManifestServer problem updating'
+                                    , 'source:', sourceId
+                                    , 'force:', force
+                                    , 'error:', err
+                                    , 'CAUTION: Error is suppressed!');
+        });
 };
 
 
@@ -124,8 +186,7 @@ _p._wrapFamilyData = function(filesData) {
     filesData.sort(sortFilesData)
              .map(makeFile)
              // equals filesMessage.addFiles(file);
-             .forEach(filesMessage.addFiles, filesMessage);
-
+             .forEach(file=>filesMessage.addFiles(file));
     return filesMessage;
 };
 
@@ -171,7 +232,7 @@ _p._dispatchFamilyJob = function(sourceid, familyName, cacheKey, metadata) {
     timestamp.fromDate(new Date());
     job.setDate(timestamp);// Timestamp => hoping that this is the way to do it
     if(metadata)
-        job.setMetdata(JSON.stringify(metadata));
+        job.setMetadata(JSON.stringify(metadata));
 
     buffer = new Buffer(job.serializeBinary());
     return this._sendAMQPMessage(this._manifestMasterJobQueueName, buffer);
@@ -179,11 +240,30 @@ _p._dispatchFamilyJob = function(sourceid, familyName, cacheKey, metadata) {
 
 _p._dispatchFamily = function(sourceid, familyName, filesData, metadata) {
     var filesMessage = this._wrapFamilyData(filesData); // => filesMessage
-    return this._cacheFamily(filesMessage) // => cacheKey
-                    .then(cacheKey => {
-                        return this._dispatchFamilyJob(sourceid, familyName, cacheKey, metadata);
-                    })
-                    ;
+    return this._queue('cache', () => this._cacheFamily(filesMessage)) // => cacheKey
+        .then(cacheKey => {
+            return this._dispatchFamilyJob(sourceid, familyName
+                                                    , cacheKey, metadata);
+        });
+};
+
+_p._queue = function(name, job) {
+    var job_, name_, queue;
+    if(typeof name === 'function') {
+        job_ = name;
+        name_ = 'default';
+    }
+    else {
+        job_ = job;
+        name_ = name;
+    }
+
+    queue = this._queues.get(name_);
+    if(!queue) {
+        queue = new AsynQueue();
+        this._queues.set(name_, queue);
+    }
+    return queue.schedule(job_);
 };
 
 // ManifestService implementation
