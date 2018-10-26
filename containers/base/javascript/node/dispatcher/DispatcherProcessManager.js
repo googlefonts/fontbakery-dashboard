8a95146630dbcbf39396f29ec56ec9532d0ad1d4
Lasse Fister <commander@graphicore.de>
2018-10-26 03:51:23 +0200

    [PRDispatcher] WIP DispatcherProcessManager improve imports/setup

--- containers/base/javascript/node/dispatcher/DispatcherProcessManager.js
+++ containers/base/javascript/node/dispatcher/DispatcherProcessManager.js
@@ -2,13 +2,15 @@
 "use strict";
 /* jshint esnext:true, node:true*/
 
-const { ProcessManager } = require('./dispatcher/framework/ProcessManager')
-  , { FamilyPRDispatcherProcess } = require('./dispatcher/FamilyPRDispatcherProcess')
+const { ProcessManager } = require('./framework/ProcessManager')
+  , { FamilyPRDispatcherProcess } = require('./FamilyPRDispatcherProcess')
   ;
 
 if (typeof require != 'undefined' && require.main==module) {
     var { getSetup } = require('../util/getSetup')
-      , setup = getSetup(), processManager, port=50051;
+      , setup = getSetup(), processManager, port=50051
+      , secret = "// TODO: define secret!"
+      ;
 
     for(let i=0,l=process.argv.length;i<l;i++) {
         if(process.argv[i] === '-p' && i+1<l) {
@@ -20,11 +22,10 @@ if (typeof require != 'undefined' && require.main==module) {
     }
     setup.logging.info('Init server, port: '+ port +' ...');
     setup.logging.log('Loglevel', setup.logging.loglevel);
+    if(!secret.length || secret.indexOf('TODO:') !== -1)
+        setup.logging.warning('You really should define a proper secret');
     // Combine ProcessManager and the process definition:FamilyPRDispatcherProcess.
-    processManager = new ProcessManager(setup.logging, setup.db, port, secret, FamilyPRDispatcherProcess);
-    processManager.serve()
-        .catch(err => {
-            setup.logging.error('Can\'t initialize server.', err);
-            process.exit(1);
-        });
+    processManager = new ProcessManager(setup.logging, setup.db, port
+                                    , secret, FamilyPRDispatcherProcess);
+    processManager.serve();
 }
