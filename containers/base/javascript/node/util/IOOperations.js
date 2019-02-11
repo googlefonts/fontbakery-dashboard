86c2d77ffa18572dea7ac1b9bfb3f5ef0e3ae4f5
Lasse Fister <commander@graphicore.de>
2019-02-11 18:06:19 +0100

    [PRDispatcher] WIP InitWorkers initial commit, delete CleanupJobs, remove methods from IOOperations.

--- containers/base/javascript/node/util/IOOperations.js
+++ containers/base/javascript/node/util/IOOperations.js
@@ -17,7 +17,6 @@ function IOOperations(logging, dbSetup, amqpSetup) {
     this._r = null;
     this._dbTables = this._dbSetup && this._dbSetup.tables || {};
     this._amqp = null;
-    this._distributorQueueName = 'fontbakery-worker-distributor';
 }
 
 var _p = IOOperations.prototype;
@@ -97,23 +96,6 @@ _p.insertDoc = function(dbTable, doc) {
         });
 };
 
-/**
- * a family-test document
- */
-_p._createFamilyDoc = function(environment_version, test_data_hash) {
-    var doc = {
-          created: new Date()
-        , test_data_hash: test_data_hash
-        , environment_version: environment_version
-    };
-
-    return this.insertDoc('family', doc)
-        .then(function(dbResponse) {
-            var docid = dbResponse.generated_keys[0];
-            return docid;
-        });
-};
-
 _p.getLatesCollectionEntry = function(collection_id, family_name) {
     return this.query('collection')
             .getAll([collection_id, family_name], {index:'collection_family'})
@@ -125,56 +107,6 @@ _p.getLatesCollectionEntry = function(collection_id, family_name) {
             .run()
             .then(entries=>entries[0]) // => entry or undefined
             ;
-};
-
-
-/**
- * If not present, create the doc
- *
- * CAUTION: This is prone to race conditions, we may end up with
- * multiple entries that have the [ENVIRONMENT_VERSION, test_data_hash]
- * key. That is not too bad, though, since the contents should be identical
- * all we did is running an identical test multiple times.
- *
- * Enforcing uniqueness here using rethink is possible but not straight
- * forward at this point:
- *
- * https://rethinkdb.com/api/javascript/table_create/:
- *      "The data type of a primary key is usually a string (like a UUID) or a
- *       number, but it can also be a time, binary object, boolean or an array.
- *       Data types can be mixed in the primary key field, but all values must
- *       be unique. Using an array as a primary key causes the primary key
- *       to behave like a compound index;"
- *
- * TODO:
- * - use the primary index:
- *   doc = {
- *        id: [ENVIRONMENT_VERSION, test_data_hash]
- *      , created: new Date()
- *   }
- * - make sure docid can be an array everwhere
- *      OR use something like id: [ENVIRONMENT_VERSION, test_data_hash].join(':')
- * - handle cases where inserting fails because of the uniqueness constraint!
- *   Be careful an don't forget to delete the cache entry!
- * - remove the env_hash secondary index
- */
-_p.getDocId = function(test_data_hash) {
-    // I'm using this envirionment variable directly here in the hope
-    // it is kept up to date by kubernetes and thus that we don't need to
-    // restart the pods when it is updated.
-    var environment_version = process.env.ENVIRONMENT_VERSION;
-
-    return this.query('family')
-        .getAll([environment_version, test_data_hash], {index:'env_hash'})
-        .run()
-        .then(function(docs) {
-            if(docs.length)
-                // it already exists!
-                return [false, docs[0].id, environment_version];
-            return this._createFamilyDoc(environment_version, test_data_hash)
-                       .then(docid => [true, docid, environment_version]);
-        }.bind(this))
-        ;
 };
 
 _p.sendQueueMessage = function (queueName, message) {
@@ -191,17 +123,6 @@ _p.sendQueueMessage = function (queueName, message) {
     return this.amqp.channel.assertQueue(queueName, {durable: true})
            .then(sendMessage.bind(this))
            ;
-};
-
-_p.dispatchFamilyJob = function(cacheKey, docid) {
-    this._log.debug('dispatchFamilyJob:', docid);
-    var job = new FamilyJob()
-     , buffer
-     ;
-    job.setDocid(docid);
-    job.setCacheKey(cacheKey);
-    buffer = new Buffer(job.serializeBinary());
-    return this.sendQueueMessage(this._distributorQueueName, buffer);
 };
 
 _p.queueListen = function(channelName, consumer) {
