887f4b3349fc12e67b9f931cf04bfeff26007742
Lasse Fister <commander@graphicore.de>
2019-12-17 14:44:49 +0100

    WIP: allow change-feeds and single list results via the same interface.

--- containers/base/javascript/browser/js/DispatcherLists.js
+++ containers/base/javascript/browser/js/DispatcherLists.js
@@ -73,32 +73,36 @@ define([
     /**
      * send the query to subscribe-dispatcher-list
      */
-    _p._sendQuery = function(query) {
-        return this._queryAPI.subscribeTo(this, query, this._renderList.bind(this));
+    _p._sendQuery = function(query, asChangeFeed) {
+        return this._queryAPI.subscribeTo(this
+                            , query
+                            , asChangeFeed
+                            , this._renderList.bind(this));
     };
 
     /**
      * Change the List
      */
     _p._queryList = function() {
-        var query = this._getQuery();
-        return this._sendQuery(query)
-        .then(canonicalQuery=>{
-            // roomId stuff is handled by parent. This controller
-            // doesn't know.
-            // 3. if the roomId stays the same, we can stay in the room
-            // 4. if the roomId changes we leave the old room and enter the new
-            //    room
+        var query = this._getQuery()
+          , asChangeFeed = true
+          ;
+
+        // In any case, this should clear the contents, for ease
+        // of use. Some subscriptions don't produce any output and
+        // hence never report anything.
+        this._renderList([]);
+
+        return this._sendQuery(query, asChangeFeed)
+        .then(({canonicalQuery, isChangeFeed})=>{
+            // Room subscription stuff is handled by parent.
+            // This subscriber can only be in one room at a time.
 
             //The canonical query may be changed (via server response),
             // change the input field and maybe also the window URL.
             // this._queryField.value = canonicalQuery.user;
-            this._log.info('Got callback', canonicalQuery.user, canonicalQuery.url);
-
-            // In any case, this should load/reload the room contents, for ease
-            // of use.
-            // this._renderList(listData);
-            // live change events may be implemented later.
+            this._log.info(`Got callback isChangeFeed ${isChangeFeed}`
+                            , canonicalQuery.user, canonicalQuery.url);
         });
     };
 
@@ -122,19 +126,19 @@ define([
         this._session = null;
 
         this._socket = socket;
-        this._rooms = new Map(); //(roomId=>set subscribers)
+        this._rooms = new Map(); //(roomId=>{set attendees, string: canonical query})
         this._subscribers = new Map(); // subscriber => {roomId, onData}
 
-        var listener  = this._onChangeList.bind(this);
-        this._socket.on('changes-dispatcher-list', listener);
+        this._socketListener = this._onChangeList.bind(this);
+        this._socket.on('changes-dispatcher-list', this._socketListener);
+        this._reconnectHandler = this._onReconnect.bind(this);
+        this._socket.on('reconnect', this._reconnectHandler);
     }
     var _p = DispatcherListsController.prototype;
 
     _p.destroy = function(e) {
         // jshint unused:vars
-        // the way this is called, there are probably no more subscribers
-        // but we can still try to unsubscribe any remainders.
-
+        console.info('DispatcherListsController received destroy event.');
         for(let subscriber of this._subscribers.keys()) {
             // Is this the right semantic, to destroy subscribers?
             // It' probably more common to destroy children in a
@@ -144,13 +148,20 @@ define([
             // Will unsbscribe itself!
             subscriber.destroy();
         }
+        this._subscribers.clear();
 
-        console.info('DispatcherListsController received destroy event.');
-        for (let roomId of this._roomIds) {
+        for (let roomId of this._rooms.keys()) {
+            // There shouldn't be left much to do, since the
+            // subscribers called unsubscribe themselves. However,
+            // this is the essential dependency on the server,
+            // unsubscribing just to be sure.
             this._socket.emit('unsubscribe-dispatcher-list', roomId);
         }
-        this._roomIds.clear();
-        this._socket.off('changes-dispatcher-list', this._onChangeList.bind(this));
+        this._rooms.clear();
+        this._socket.off('changes-dispatcher-list', this._socketListener);
+        this._socketListener = null;
+        this._socket.off('reconnect', this._reconnectHandler);
+        this._reconnectHandler = null;
     };
 
     // This should be forwarded to the single room handler, because
@@ -162,12 +173,11 @@ define([
     _p._onChangeList = function(roomId, data) {
         var room = this._rooms.get(roomId);
         console.log('_onChangeList for', roomId, 'data', data);
-        for(let subscriber of room) {
+        for(let subscriber of room.attendees) {
             console.log('subscriber', subscriber);
             let {onData} = this._subscribers.get(subscriber);
             onData(data);
         }
-        // WHEN?: this._socket.emit('unsubscribe-dispatcher-list', {});
     };
 
     _p._getElementFromTemplate = function(className) {
@@ -180,86 +190,109 @@ define([
     };
 
     _p.unsubscribe = function(subscriber) {
-
+        var subscription = this._subscribers.get(subscriber)
+          , roomId, room
+          ;
+        if(!subscription) {
+            // something went wrong
+            console.error('Calling unsubscribe but there\'s no subscription',
+                ' for', subscriber);
+            return;
+        }
+        this._subscribers.delete(subscriber);
+        roomId = subscription.roomId;
+        room = this._rooms.get(roomId);
+        room.attendees.delete(subscriber);
+        if(!room.attendees.size) {
+            this._socket.emit('unsubscribe-dispatcher-list', roomId);
+            this._rooms.delete(roomId);
+        }
     };
 
-    _p.subscribeTo = function(subscriber, query, onData) {
-        return new Promise((resolve, reject)=>{
-            var callback = (result, error)=>{
-                if(error) {
-                  this._log.error(`Can\'t subscribe to dispatcher list: ${error}`);
-                  reject(error);
-                  return;
-                }
-                // canonicalQuery should update the query string sent
-                // by the user interface and, if that's a thing, should
-                // be placed into the query parameter of the url.
-                var {roomId, canonicalQuery} = result
-                  , room = this._rooms.get(roomId)
-                  ;
-                if(!room) {
-                    room = new Set();
-                    this._rooms.set(roomId, room);
+    _p._onReconnect = function() {
+        function callback(roomId, result, error){
+            if(error) {
+                console.error('Can\'t resubscribe to dispatcher list '
+                            + `for room ${roomId} with:`, error);
+                return;
+            }
+        }
+        for(let [roomId, room] of this._rooms) {
+            this._socket.emit('subscribe-dispatcher-list'
+                            , {queryString:room.query, asChangeFeed: true}
+                            ,  callback.bind(this, roomId));
+        }
+    };
 
-                }
-                console.info('callback:subscribe-dispatcher-list'
-                              , `roomId: ${roomId}`
-                              , `canonicalQueryString: ${canonicalQuery.user}`);
-                this._subscribers.set(subscriber, {roomId, onData});
-                room.add(subscriber);
-                resolve(canonicalQuery);
-            };
+    _p.subscribeTo = function(subscriber, query, asChangeFeed, onData) {
+        function callback(resolve, reject, result, error){
+            //jshint validthis:true
+            // In some cases we don't have a change feed as a result,
+            // just a list of entries, we handle both cases in here!
+            if(error) {
+              console.error('Can\'t subscribe to dispatcher list:', error);
+              reject(error);
+              return;
+            }
+
+            // canonicalQuery should update the query string sent
+            // by the user interface and, if that's a thing, should
+            // be placed into the query parameter of the url.
+            var {roomId, canonicalQuery, isChangeFeed, data} = result;
+
+            console.info('callback:subscribe-dispatcher-list'
+                          , `roomId: ${roomId}`
+                          , `canonicalQueryString: ${canonicalQuery.user}`
+                          , `isChangeFeed: ${isChangeFeed}`
+                          , 'data:', data);
+            if(!isChangeFeed) {
+                // assert roomId === null
+                resolve(result);// has: {canonicalQuery, isChangeFeed}
+                onData(data);
+                return;
+            }
+            // is a change feed
+            // assert data === null
+            var room = this._rooms.get(roomId);
+            if(!room) {
+                room = {
+                    attendees: new Set()
+                  , query: canonicalQuery.user
+                };
+                this._rooms.set(roomId, room);
+            }
+
+            if(this._subscribers.has(subscriber)) {
+                let oldSubscription = this._subscribers.get(subscriber);
+                if(oldSubscription.roomId !== roomId)
+                    // If the roomId changes we leave the old room
+                    // and enter the new room, Only do this if roomId is
+                    // different
+                    this.unsubscribe(subscriber);
+            }
+            // If subscriber was subscribed and roomId is the same
+            // roomId is the same, this should be just fine.
+            // onData will be replaced, but the subscriber is
+            // expected to be cool with this.
+            this._subscribers.set(subscriber, {roomId, onData});
+            room.attendees.add(subscriber);
+            resolve(result);// has: {canonicalQuery, isChangeFeed}
+        }
+
+        return new Promise((resolve, reject)=>{
             // enters "the room"
-            this._socket.emit('subscribe-dispatcher-list', {query}, callback);
+
+            // asChangeFeed: some queries can't be a change feed, these
+            // will ignore this flag and always return `isChangeFeed=False`
+            // but all queries that can be change feeds can also be not
+            // and for these this flag is effective.
+            // TODO: we should maybe rename the event to:
+            //              {"query" or "get"}-dispatcher-list
+            this._socket.emit('subscribe-dispatcher-list',
+                    {queryString:query, asChangeFeed: !!asChangeFeed},
+                    callback.bind(this, resolve, reject));
         });
     };
-
-    // _p._showProcess = function(processId) {
-    //     var processElem = dom.createElement('div')
-    //       , listener = this._onChangeProcess.bind(this, processElem)
-    //       , subscriptionRequest = 'subscribe-dispatcher-process'
-    //       , subscribe = null
-    //       , reconnectHandler = (attemptNumber) => {
-    //           if(subscribe === null) return;
-    //           console.log('socket on reconnect', '#'+attemptNumber+':', subscriptionRequest);
-    //           subscribe();
-    //         }
-    //       , destructor = (e)=>{
-    //             //jshint unused:vars
-    //             this._currentProcessListener = null;
-    //             this._currentProcessLastData = null;
-    //
-    //             this._container.removeEventListener('destroy', destructor, false);
-    //             this._log.info('OH, Hey!, the destroy event got received');
-    //
-    //             this._socket.off('changes-dispatcher-process', listener);
-    //             if(processId) {
-    //                 subscribe = null;
-    //                 this._socket.off('reconnect', reconnectHandler);
-    //                 this._socket.emit('unsubscribe-dispatcher-process', {
-    //                     processId: processId
-    //                 });
-    //             }
-    //         }
-    //       ;
-    //
-    //     this._clearContainer();
-    //     this._currentProcessListener = listener;
-    //     this._currentProcessLastData = null;
-    //
-    //     dom.appendChildren(this._container, [this._log.container, processElem]);
-    //     this._log.reatached();
-    //
-    //     this._socket.on('changes-dispatcher-process', listener);
-    //     this._container.addEventListener('destroy', destructor, false);
-    //     processElem.addEventListener('destroy', destructor, false);
-    //     if(processId) {
-    //         this._socket.on('reconnect', reconnectHandler);
-    //         subscribe = ()=>this._socket.emit(subscriptionRequest
-    //                                             , { processId: processId});
-    //         subscribe();
-    //     }
-    // };
 
     return {
         DispatcherListsController
